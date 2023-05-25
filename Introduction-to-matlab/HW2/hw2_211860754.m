c6a7d14a9173fda48230aeb265d4ebc079a250dc
Yuval Bernard <yuval.bernard@weizmann.ac.il>
2023-05-25 13:48:26 +0300

    .

--- Introduction-to-matlab/HW2/hw2_211860754.m
+++ Introduction-to-matlab/HW2/hw2_211860754.m
@@ -1,9 +1,11 @@
 load data_hw2_2023b.mat
 %% Q1
+% Calculate the average and its index.
 avg = mean(results);
 [~,I] = max(avg);
 fprintf('Day %d had the highest average score', I)
 %% Q2
+% Make non-valid results equal to zero and recalculate mean.
 clearvars -except valid_results results experimental_group norm_day_coeff
 avg_valid = mean(results .* valid_results);
 [~,I_valid] = max(avg_valid);
@@ -21,6 +23,7 @@ fprintf('Day %d has the highest score.\nThe value is %f',col,M)
 %% Q4
 clearvars -except valid_results results experimental_group norm_day_coeff
 avg_per_row = mean(results, 2);
+% Sort avg_per_row in ascending order, and display the first 4 values.
 [avg_sorted,I] = sort(avg_per_row);
 fprintf('Animal %d had average score %d.\n', [I(1:4), floor(avg_sorted(1:4))].')
 %% Q5
@@ -32,12 +35,15 @@ fprintf('Mean of animals s.d: %f', mean(std_per_animal));
 fprintf('Larger mean s.d: %f', max([mean(std_per_day), mean(std_per_animal)]))
 %% Q6
 clearvars -except valid_results results experimental_group norm_day_coeff
+% calculate diff along row and return index of min.
 [~,I] = min(mean(diff(results,1,2)));
 fprintf('Animal %d had the slowest average improvement.', I)
 %% Q7
 clearvars -except valid_results results experimental_group norm_day_coeff
 fprintf('Animal %d\n', 1:2:29)
 %% Q8
+% matrix multiplication performs the requested action. @ceil rounds number
+% up.
 clearvars -except valid_results results experimental_group norm_day_coeff
 fprintf('Animal %d: %d\n',[1:1:30; ceil(results * norm_day_coeff).'])
 %% Q9
@@ -45,7 +51,7 @@ clearvars -except valid_results results experimental_group norm_day_coeff
 group_1_mean_per_day = mean(results(experimental_group == 1,:));
 group_2_mean_per_day = mean(results(experimental_group == 2,:));
 fprintf('Day %d: group %d had higher mean score\n', ...
-    [1:10;(group_2_mean_per_day > group_1_mean_per_day) + 1])
+    [1:10; (group_2_mean_per_day > group_1_mean_per_day) + 1])
 %% Q10
 clearvars -except valid_results results experimental_group norm_day_coeff
 [~,cols] = size(results);
